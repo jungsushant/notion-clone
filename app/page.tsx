16f33fcfa7a9f348d43781be336d1f680e588c22
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-15 21:33:26 +0545

    trunk added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello Notion Clone</p>;
+  return (
+    <div>
+      <Button variant="outline">Click Me</Button>
+    </div>
+  );
 }
