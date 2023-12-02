a1b7d0c59c4d7e61f102b32a4bd910b105689c6b
Bikrant <bikrantjung1214@gmail.com>
2023-12-03 00:18:50 +0545

    auth and database done

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +17,12 @@ export const Heading = () => {
         Jotion is the conected workspace where <br /> better, faster work
         happens.
       </h3>
-      <Button>
-        Enter Jotion <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && <Spinner size="lg" />}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          Enter Jotion <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import ConvexReactClientProvider from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,7 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
           storageKey="jotion-theme-2"
         >
-          {children}
+          <ConvexReactClientProvider>{children}</ConvexReactClientProvider>
         </ThemeProvider>
       </body>
     </html>
